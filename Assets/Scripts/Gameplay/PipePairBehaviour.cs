4d1a37198c85160cec5381e382fc5975e2c63a0f
Jeremy J. Flowers <rpgjuwangaming@gmail.com>
2021-03-29 16:49:38 -0500

    Programming FlappyBird in Unity

---
+++ Assets/Scripts/Gameplay/PipePairBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipePairBehaviour : MonoBehaviour
+{
+    public float DistanceBetweenPipes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Input/FlapBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlapBehaviour : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    public float FlapForce = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Flap"))
+            _rigidbody.AddForce(new Vector3(0, FlapForce, 0), ForceMode.Impulse);
+    }
+}
